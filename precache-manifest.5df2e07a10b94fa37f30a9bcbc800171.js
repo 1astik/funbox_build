7973c2bf0c838b8658c1249f97060e36bf0e1211
Mark <mark.senokosov@quantumtec.tech>
2020-08-21 15:27:01 +0300

    Initial commit

---
+++ precache-manifest.5df2e07a10b94fa37f30a9bcbc800171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024d35899a10cf9ec9c7bd8230312b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4405bd6f5f06af5423d",
+    "url": "/static/css/main.86817074.chunk.css"
+  },
+  {
+    "revision": "f0eb58945a863534aa59",
+    "url": "/static/js/2.06b5f6a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06b5f6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4405bd6f5f06af5423d",
+    "url": "/static/js/main.56abe605.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
